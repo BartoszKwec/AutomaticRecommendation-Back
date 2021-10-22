e54da51c610a312ba8a4957a41fe09b859cdc328
barto <bartosz.kwec@vp.pl>
2021-10-22 16:22:41 +0200

    Git init

---
+++ src/test/java/pl/kwec/automaticrecommendationback/AutomaticRecommendationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kwec.automaticrecommendationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaticRecommendationBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
